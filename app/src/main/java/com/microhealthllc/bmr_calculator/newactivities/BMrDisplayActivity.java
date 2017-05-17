3627ee2a0a4dd66d076121efcdb49f192b32c96c
boatengfrankenstein <boatengfrankenstein@gmail.com>
2017-05-17 15:04:25 -0400

    Fragments and views

---
+++ app/src/main/java/com/microhealthllc/bmr_calculator/newactivities/BMrDisplayActivity.java
@@ -0,0 +1,15 @@
+package com.microhealthllc.bmr_calculator.newactivities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.microhealthllc.bmr_calculator.R;
+
+public class BMrDisplayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bmr_display);
+    }
+}
